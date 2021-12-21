c9f5787acfed2df495794eea82c7311f3cf88eb3
Joaquin2000zz <joaquin2000zz@gmail.com>
2021-12-21 08:51:23 -0800

    ended function that determines if a random number is grater than 5 or less than 6 and not zero

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number > 0:
 else:
     aux = number * -1
 if (aux % 10) > 5:
-    print("Last digit of {} is {} and is\
- greater than 5".format(number, number % 10))
+    print("Last digit of {} is {} and is \
+greater than 5".format(number, number % 10))
 elif (aux % 10) < 6 and (aux % 10) != 0:
-    print("Last digit of {} is {} and is\
- less than 6 and not 0".format(number, number % 10))
+    print("Last digit of {} is {} and is \
+less than 6 and not 0".format(number, number % 10))
 else:
     print("Last digit of {} is {} and is 0".format(number, number % 10))
