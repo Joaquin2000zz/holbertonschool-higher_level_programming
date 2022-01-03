904054bb4199f288a53fcbaeeaea94df12c9b285
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-03 08:47:28 -0800

    changed if statement again

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if len(my_list) <= idx and idx <= 0:
+    if len(my_list) > idx and idx >= 0:
+        my_list[idx] = element
         return (my_list)
     else:
-        my_list[idx] = element
         return (my_list)
