7a34e99e258bcd043bc44049480fcf568d6ce863
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-03 08:14:51 -0800

    if statement changed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if my_list[idx] is True and idx > 0:
+    if my_list[idx] is not 0 and idx > 0:
         return (my_list.pop(idx))
     else:
         return (None)
