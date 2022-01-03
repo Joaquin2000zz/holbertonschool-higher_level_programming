439948f1689c18abd1280571ee82b6b2feb285ed
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-03 08:25:27 -0800

    fixed syntax error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if len(my_list) >= idx and idx >= 0:
+    if len(my_list) > idx and idx >= 0:
         return (my_list[idx])
     else:
         return (None)
