e396f1ac336ceaf7e5c159eb88b4ec40e31a1ebc
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-03 08:52:19 -0800

    trying to solve an requirement error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@
 def replace_in_list(my_list, idx, element):
     if len(my_list) > idx and idx >= 0:
         my_list[idx] = element
-        return (my_list)
+        return my_list
     else:
-        return (my_list)
+        return my_list
