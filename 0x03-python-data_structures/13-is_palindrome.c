25488f486f325a8519f06307b2d4ffb6af79ddc6
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-03 16:49:11 -0800

    Write a function in C that checks if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * is_palindrome - function that checks if a singly linked list is a palindrome
+ * @head: the linked list
+ * Return: 1 if it is or 0 if is not
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *reflection, *current;
+
+	if (!*head)
+		return (1);
+	reflection = malloc(sizeof(listint_t));
+	if (!reflection)
+	{
+		free(reflection);
+		return (0);
+	}
+	current = *head;
+	while (current->next)
+	{
+		add_nodeint(&reflection, current->n), current = current->next;
+	}
+	current = *(head);
+	while (current)
+	{
+		if (reflection->n == current->n)
+		{
+			freelistint(reflection);
+			return (1);
+		}
+		current = current->next, reflection = reflection->next;
+	}
+	return (0);
+}
+
+/**
+ * add_nodeint - adds a new node at the end of a listint_t list.
+ * @head: the head of the list
+ * @n: the node's content
+ * Return: the new header's list
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_header;
+
+	new_header = malloc(sizeof(listint_t));
+
+	if (!new_header)
+	{
+		free(new_header);
+		return (0);
+	}
+
+	new_header->n = n;
+	new_header->next = *head;
+
+	*head = new_header;
+
+	return (*head);
+}
