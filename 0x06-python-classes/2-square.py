945d6a75a71db96fbfa0bf5f7da903136ba44a12
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-18 12:03:22 -0800

    fixed documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
+"""square 2"""
+
+
 class Square:
+    """square class"""
     def __init__(self, size=0):
+        """method init"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif int(size) < 0:
