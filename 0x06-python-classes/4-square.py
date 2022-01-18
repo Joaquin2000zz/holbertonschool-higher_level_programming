08a89b40d5f4ed238dbb6aa38f3802b88cd8de88
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-18 08:33:03 -0800

    writed class Square that defines a square and has a setter and getter as methods

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, n):
+        if type(n) is not int:
+            raise TypeError("size must be an integer")
+        if int(n) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = n
+
+    def area(self):
+        return self.__size * self.__size
