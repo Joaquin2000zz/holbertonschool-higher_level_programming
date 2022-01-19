a86cc7a8fe6243f5a22974d78e9dc4e3578f1714
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-19 13:38:09 -0800

    added if instance to check if the tuple has to values

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,7 +15,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(y) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(y):
+        if y[0] == '' or y[1] == '':
             raise TypeError("position must be a tuple of 2 positive integers")
         if y[0] < 0 or y[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
