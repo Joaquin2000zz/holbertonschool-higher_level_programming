eaad3372eb58f993527c0f70cafe6ef1364b9cd5
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-24 14:41:29 -0800

    fixed errors

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -10,6 +10,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """init method"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if int(width) < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if int(height) < 0:
+            raise ValueError("height must be >= 0")
         self.height = height
         self.width = width
         Rectangle.number_of_instances += 1
