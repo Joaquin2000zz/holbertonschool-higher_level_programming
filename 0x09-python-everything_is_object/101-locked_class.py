4697ab043ca5fa2d654dda3a0b65c7c9b79dc6ea
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-25 13:50:43 -0800

    fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+advanced
+"""
+
+
 class MyClass(object):
+    """
+    function which set the only name permited to create an instance class
+    """
+
     __slots__ = ['first_name']
+
     def __init__(self, first_name):
         pass
