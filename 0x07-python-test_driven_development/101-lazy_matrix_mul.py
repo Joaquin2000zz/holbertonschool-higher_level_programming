07598d0cee3b1698e86e3eb1d0ae8b01946508b7
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-26 08:45:02 -0800

    added 101 advanced which uses numpy to multiply matrices

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module:
+101-lazy_matrix_mul
+contains lazy_matrix_mul
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    lazy_matrux_mul
+    """
+    return numpy.dot(m_a, m_b)
