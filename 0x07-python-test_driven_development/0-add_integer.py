ffb46dd4b0155734f5b9d0fde82506032fe65809
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-25 19:36:07 -0800

    fixed documentation added doc to raisers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""0-add_integer that adds 2 integers
-add_integer function
-@a: first number
-@b: second number
+"""
+0-add_integer that adds 2 integers. add_integer function
+@a: first number @b: second number
 Return: a + b
 """
 
 
 def add_integer(a, b=98):
     """add_integer function which returns a + b
-    @a: first number
-    @b: second number
+    @a: first number @b: second number
     """
+    """raisers. checking types"""
     if type(a) is not int:
         if type(a) is not float:
             raise TypeError("a must be an integer")
