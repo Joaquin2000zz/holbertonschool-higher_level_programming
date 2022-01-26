b9ecd6f24d1e495e75089155e2efa8ed84b1f28a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-25 19:23:48 -0800

    fixed bad position of documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,13 +3,15 @@
 add_integer function
 @a: first number
 @b: second number
-Return: a + b"""
+Return: a + b
+"""
 
 
 def add_integer(a, b=98):
     """add_integer function which returns a + b
     @a: first number
-    @b: second number"""
+    @b: second number
+    """
     if type(a) is not int:
         if type(a) is not float:
             raise TypeError("a must be an integer")
