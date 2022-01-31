4fe8954e71c7a6ae6079db581d3e908029468f94
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-31 13:35:58 -0800

    added for to check if the list values are integers

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,4 +12,7 @@ class MyList(list):
         """
         prints a sorted list
         """
+        for item in self:
+            if type(item) is not int:
+                raise ValueError("Must be a list of integers")
         print(sorted(self))
