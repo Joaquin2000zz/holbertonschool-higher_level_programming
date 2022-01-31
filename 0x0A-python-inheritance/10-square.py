0db49d938d1d7859fcb1072f183cce83d96946ba
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-01-31 14:20:04 -0800

    changed output message

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,55 +2,10 @@
 """
 8-base_geometry.py module
 """
+Rectangle = __import__('8-rectangle').Rectangle
 
 
-class BaseGeometry:
-    """
-    class BaseGeometry
-    """
-    def area(self):
-        """
-        raises an Exception with the message area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        integer_validator method
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """
-    class Rectangle
-    """
-    def __init__(self, width, height):
-        """
-        init method
-        """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """
-        area method
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        the str method
-        """
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
-
-
-class Square(BaseGeometry):
+class Square(Rectangle):
     """
     Square class
     """
@@ -66,3 +21,9 @@ class Square(BaseGeometry):
         area method
         """
         return self.__size ** 2
+
+    def __str__(self):
+        """
+        the str method
+        """
+        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
