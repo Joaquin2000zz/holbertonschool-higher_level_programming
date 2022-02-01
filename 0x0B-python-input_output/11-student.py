fe6414639d546137bb7df64a0a7a0edea682d705
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-02-01 13:37:04 -0800

    added a dict of the json

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,7 +26,6 @@ class Student:
         """
         replaces all attributes of the Student instance
         """
-        jsonDict = dict(json)
         for key in dict(self.__dict__):
             if key in json:
-                self.__dict__[key] = jsonDict[key]
+                self.__dict__[key] = json[key]
