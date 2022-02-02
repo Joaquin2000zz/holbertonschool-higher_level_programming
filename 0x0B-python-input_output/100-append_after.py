9849960eec8322e64bf933cf1f8204b31b0cfc58
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-02-01 16:10:37 -0800

    first commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+100-append_after module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    inserts a line of text to a file, after each line containing a string
+    """
+    flag = 0
+    newContent = ""
+    with open(filename, 'r+', encoding="utf-8") as f:
+        for line in f:
+            newContent += line
+            if search_string in line:
+                newContent += new_string
+        f.write(newContent)
