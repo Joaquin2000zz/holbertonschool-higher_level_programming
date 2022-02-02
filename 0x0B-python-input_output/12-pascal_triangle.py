38da5e240708e8b13e62baa497a2749ceb856041
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-02-02 08:49:49 -0800

    fixed wrong append

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,16 +10,17 @@ def pascal_triangle(n):
     """
     ret = []
     auxPrev = []
+    aux = [1]
     for i in range(0, n):
         if i == 0:
-            ret.append(1)
+            ret.append(aux)
             continue
         aux = []
         for j in range(0, i + 1):
             if j == 0 or j == i:
                 aux.append(1)
                 continue
-            aux.append(auxPrev[j] + auxPrev[1 - j])
+            aux.append(int(auxPrev[j]) + int(auxPrev[1 - j]))
         ret.append(aux)
         auxPrev = aux
     return ret
