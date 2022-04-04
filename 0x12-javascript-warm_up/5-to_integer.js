bf86a557a4c88afc6d97bd1dab123be967060b85
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-04 11:08:29 -0700

    checked existence of the argument to make the operations

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,11 @@
 
 const { argv } = require('process');
 
-if (!isNaN(argv[2])) {
-  argv[2] = parseInt(argv[2]);
-  console.log('My number ' + argv[2]);
-} else {
-  console.log('Not a number');
+if (argv[2]) {
+  if (!isNaN(argv[2])) {
+    argv[2] = parseInt(argv[2]);
+    console.log('My number ' + argv[2]);
+  } else {
+    console.log('Not a number');
+  }
 }
