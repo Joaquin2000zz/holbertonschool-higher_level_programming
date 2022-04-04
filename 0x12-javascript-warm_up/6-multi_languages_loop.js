5c3fec6354bad4a1b4a8d205e648a2b737813d33
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-04 11:34:28 -0700

    fixed semistandar

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const list = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; list[i]; i++)
-{
+for (let i = 0; list[i]; i++) {
   console.log(list[i]);
 }
