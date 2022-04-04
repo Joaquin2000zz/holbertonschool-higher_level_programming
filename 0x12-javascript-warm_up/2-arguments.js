a0d480d5b7907454548fff7e4b74eca2d025afc1
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-04 10:32:11 -0700

    script that prints a message depending of the number of arguments passed reading from ARGV

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const { argv } = require('process');
 if (!argv[2]) {
   console.log('No argument');
 } else if (argv[2] && !argv[3]) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments Found');
+  console.log('Arguments found');
 }
