6f181381de012ed13caaccfe969d089acd6f5972
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-04 11:16:00 -0700

    fixed semistandar

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const { argv } = require('process');
 if (argv[2]) {
   if (!isNaN(argv[2])) {
     argv[2] = parseInt(argv[2]);
-    console.log('My number ' + argv[2]);
+    console.log('My number: ' + argv[2]);
   } else {
     console.log('Not a number');
   }
