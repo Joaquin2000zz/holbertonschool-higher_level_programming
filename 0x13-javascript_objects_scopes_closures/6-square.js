f5106c260272641f1a5e97bfd003d1fadc2e927e
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-05 10:56:22 -0700

    fixed semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,8 +7,8 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-	  if (c === undefined) {
-      this.print()
+    if (c === undefined) {
+      this.print();
     } else {
       for (let i = 0; i < this.width; i++) {
         console.log(c.repeat(this.width));
