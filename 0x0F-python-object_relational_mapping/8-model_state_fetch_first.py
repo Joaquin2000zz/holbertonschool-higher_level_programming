94efd49defa428315a344a9689f68dcf8b0b756a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-07 18:42:19 -0700

    select * from states where id like 1 with orm

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    query = session.query(State).filter_by(id='99991').all()
+    if (len(query) > 0):
+        for item in query:
+            print(f'{item.id}: {item.name}')
+    else:
+        print("Nothing")
