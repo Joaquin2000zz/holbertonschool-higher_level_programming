21ad015361df25a3603637331004d03f42647d3b
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-07 19:56:53 -0700

    inserts into table with ORM

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy import insert
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    if (len(argv) == 4):
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(argv[1],
+                                       argv[2], argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+
+        session = Session(engine)
+
+        new_state = State(name='Louisiana')
+
+        session.add(new_state)
+        session.commit()
+
+        query = session.query(State).filter_by(name="Louisiana").all()
+        if (len(query) > 0):
+            for item in query:
+                print(item.id)
+        else:
+            print("Not found")
+        session.close()
