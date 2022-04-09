3ebb96e6a0b4e7c571fadd529d33fed90711612d
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-09 06:27:29 -0700

    fixed syntax error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
                              passwd=argv[2],
                              db=argv[3])
         cur = db.cursor()
-        # Print results in comma delimited format
         cmd = "SELECT * FROM states WHERE name LIKE %s"
-        cur.execute(cmd, (argv[4],)
+        cur.execute(cmd, (argv[4],))
         rows = cur.fetchall()
         for row in rows:
             print(row)
