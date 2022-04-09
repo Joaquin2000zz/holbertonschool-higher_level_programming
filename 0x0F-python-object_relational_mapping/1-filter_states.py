4d1108e80248e18fab15d26e0d6d46c6cb32e04f
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-09 13:07:21 -0700

    added try to avoid bugs

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,17 @@ from sys import argv
 
 if __name__ == "__main__":
     if (len(argv) > 3):
-        db = MySQLdb.connect(host='localhost', user=argv[1],
-                             passwd=argv[2], db=argv[3])
-        cur = db.cursor()
-        # Print results in comma delimited format
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+        try:
+            db = MySQLdb.connect(host='localhost', user=argv[1],
+                                 passwd=argv[2], db=argv[3])
+            cur = db.cursor()
+            # Print results in comma delimited format
+            cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
 ORDER BY states.id, states.name ASC;")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+            cur.close()
+            db.close()
+        except Exception:
+            pass
