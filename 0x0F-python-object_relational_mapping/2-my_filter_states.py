a85e715c12399124337d681793e4ef564cfb066f
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-09 06:40:18 -0700

    fixed pepocho

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     if (len(argv) > 3):
-        db = MySQLdb.connect(host='localhost',
+        db = MySQLdb.connect(host='localhost', port=3306,
                              user=argv[1],
                              passwd=argv[2],
                              db=argv[3])
