a33930ff41b6b044c0e2db362f049dd7c6d1636d
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-08 18:55:58 -0700

    City class which creates objects which are the ORM     representation of a SQL table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,14 +5,15 @@ script that lists all states from the database hbtn_0e_0_usa using MYSQLdb
 import MySQLdb
 from sys import argv
 
-if (len(argv) > 3):
-    db = MySQLdb.connect(host='localhost', user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    # Print results in comma delimited format
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    if (len(argv) > 3):
+        db = MySQLdb.connect(host='localhost', user=argv[1],
+                             passwd=argv[2], db=argv[3])
+        cur = db.cursor()
+        # Print results in comma delimited format
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,16 +5,17 @@ script that lists all states from the database hbtn_0e_0_usa using MYSQLdb
 import MySQLdb
 from sys import argv
 
-if (len(argv) > 3):
-    db = MySQLdb.connect(host='localhost', user=argv[1],
-                         passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    # Print results in comma delimited format
-    cmd = "SELECT cities.id AS id, cities.name AS Cities, states.name AS Sta\
-tes from cities INNER JOIN states ON cities.state_id = states.id"
-    cur.execute(cmd)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    if (len(argv) > 3):
+        db = MySQLdb.connect(host='localhost', user=argv[1],
+                             passwd=argv[2], db=argv[3])
+        cur = db.cursor()
+        # Print results in comma delimited format
+        cmd = "SELECT cities.id AS id, cities.name AS Cities, states.name A\
+S States from cities INNER JOIN states ON cities.state_id = states.id"
+        cur.execute(cmd)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
