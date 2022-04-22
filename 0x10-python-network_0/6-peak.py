2d0fae7d14098dc7325e7f5cbc54c82531d05fea
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-22 19:47:39 +0000

    Technical interview preparation: find the peak number of a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,12 @@
 """
 find_peak module
 """
+
+
 def find_peak(list_of_integers):
     """
     find_peak
     """
     if len(list_of_integers) > 0:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
