8e8f7cb2e303d5fc6513b08b430ac4a4c0bbd8e0
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-24 15:30:48 +0000

    changed f'' instead .format() and added text to the output message

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    print(f'\t- type: {type(html)}')
-    print(f'\t- content: {html}')
-    print(f'\t- utf8 content: {html.decode("UTF-8")}')
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("UTF-8")))
