93ce184376cd9ecc0f6bbc1a54ffdf7c9cb55c0e
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-25 03:00:24 +0000

    handled not a valid json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     data = {'q': argv[1] if len(argv) > 1 else ""}
     r = requests.post('http://0.0.0.0:5000/search_user',
                       data=data)
-    Rdict = r.json()
+    try:
+        Rdict = r.json()
+    except Exception as e:
+        print("Not a valid JSON")
     if len(Rdict) == 2:
-        try:
-            print("[{}] {}".format(Rdict['id'], Rdict['name']))
-        except Exception as e:
-            print("Not a valid JSON")
+        print("[{}] {}".format(Rdict['id'], Rdict['name']))
     else:
         print("No result")
