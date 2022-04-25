069e839ae5446afb937bee35d9e82e7340cd899a
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-25 02:55:48 +0000

    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+requests is the module and is used even to post data without encoding
+get to github with your user and token
+read a json taken from the response and display github id
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if (len(argv) == 3):
+        r = requests.get('https://api.github.com/user',
+                          auth=(argv[1], argv[2]))
+    try:
+        print(r.json()['id'])
+    except Exception as e:
+        print(None)
