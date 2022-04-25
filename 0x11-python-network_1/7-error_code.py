1f6eb19f2e13a16bd68632dd79e7d0c9dfd6821d
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-25 01:50:31 +0000

    print index content or error code if x.status_code greater lower than 400

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 requests is the module and is used even to post data without encoding
-print index content or error code if x.status_code greater than 399
+print index content or error code if x.status_code lower than 400
 """
 import requests
 from sys import argv
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    if (r.status_code < 399):
+    if (r.status_code < 400):
        print(r.text)
     else:
         print("Error code: {}".format(r.status_code))
