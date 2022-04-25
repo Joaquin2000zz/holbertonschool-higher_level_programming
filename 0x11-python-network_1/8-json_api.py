6b1d2070e7a6eeecd8f68fe6a0e6db3ed1d237ff
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-25 02:39:34 +0000

    read a json taken from the response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+requests is the module and is used even to post data without encoding
+read a json taken from the response
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    data = {'q': argv[1] if len(argv) > 1 else ""}
+    r = requests.post('http://0.0.0.0:5000/search_user',
+                      data=data)
+    Rdict = r.json()
+    if len(Rdict) == 2:
+        print("[{}] {}".format(Rdict['id'], Rdict['name']))
+    else:
+        print("No result")
