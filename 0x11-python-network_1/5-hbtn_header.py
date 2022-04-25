efd83ac2a85994184d128834fbbf85a9f3f81d46
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-04-25 01:05:04 +0000

    url taken from argv[1]

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,8 +4,8 @@ requests is the module and is used even to post data without encoding
 module which shows x-request-id
 """
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io')
+    r = requests.get(argv[1])
     print(r.headers['x-request-id'])
