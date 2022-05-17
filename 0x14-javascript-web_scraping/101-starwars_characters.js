9a4f2d706ed1fd66ffabcd218c9e62f6baa761c6
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 19:39:47 +0000

    now traverse all the movies and matches with the correct to then traverse for the characters list

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 const argv = process.argv;
 const axios = require('axios');
-axios.get('https://swapi-api.hbtn.io/api/films/' + argv[2] + '/')
+axios.get('https://swapi-api.hbtn.io/api/films/')
   .then(resp => {
-    for (let i = 0; resp.data.characters[i]; i++) {
-      axios.get(resp.data.characters[i])
-        .then(resp => {
-          console.log(resp.data.name);
-        });
+    for (let i = 0; resp.data.results[i]; i++) {
+      if (resp.data.results[i].episode_id === parseInt(argv[2]))
+      {
+        for (let j = 0; resp.data.results[i].characters[j]; j++)
+        {
+          axios.get(resp.data.results[i].characters[j])
+            .then(resp => {
+              console.log(resp.data.name);
+          });
+        }
+        break;
+      }
     }
   });
