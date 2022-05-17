bbb49dd95aecf5dfa2889ee7dbf96eaa1278ad0e
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 17:44:18 +0000

    script that prints the number of movies where the WedgeAntilles character is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 const argv = process.argv;
 const axios = require('axios');
-try {
-  axios.get(argv[2])
-    .then(resp => {
-      let n = 0;
-      for (let i = 0; resp.data.results[i]; i++) {
-        if (resp.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-          n++;
-        }
+axios.get(argv[2])
+  .then(resp => {
+    let n = 0;
+    for (let i = 0; resp.data.results[i]; i++) {
+      if (resp.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        n++;
       }
-      console.log(n);
-    });
-}
-catch(err)
-{
-
-}
+    }
+    console.log(n);
+  });
