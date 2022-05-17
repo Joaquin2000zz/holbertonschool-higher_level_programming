fa836c6d24b8006785a23130659009380d7a5eaa
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 17:42:31 +0000

    script that prints the number of movies where the Wedgeis present Antillescharacter

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
 const argv = process.argv;
 const axios = require('axios');
-axios.get(argv[2])
-  .then(resp => {
-    let n = 0;
-    for (let i = 0; resp.data.results[i]; i++) {
-      if (resp.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        n++;
+try {
+  axios.get(argv[2])
+    .then(resp => {
+      let n = 0;
+      for (let i = 0; resp.data.results[i]; i++) {
+        if (resp.data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+          n++;
+        }
       }
-    }
-    console.log(n);
-  });
+      console.log(n);
+    });
+}
+catch(err)
+{
+
+}
