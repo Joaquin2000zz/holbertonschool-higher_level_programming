baa29fdaef26c771e37790cb827697c1cb8491ee
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 19:53:27 +0000

    now is printing the movie which represent their respective conologic order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,8 +3,15 @@ const argv = process.argv;
 const axios = require('axios');
 axios.get('https://swapi-api.hbtn.io/api/films/')
   .then(resp => {
+    argv[2] = parseInt(argv[2]);
+    if (argv[2] >= 4) {
+      argv[2] -= 3;
+    }
+    else {
+      argv[2] += 3;
+    }
     for (let i = 0; resp.data.results[i]; i++) {
-      if (resp.data.results[i].episode_id === parseInt(argv[2]))
+      if (resp.data.results[i].episode_id === argv[2])
       {
         for (let j = 0; resp.data.results[i].characters[j]; j++)
         {
