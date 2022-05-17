637dda647bb853abc220228879fda8e4140d3e4c
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 18:27:55 +0000

    script that computes the number of tasks completed by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const argv = process.argv;
+const axios = require('axios');
+axios.get(argv[2])
+  .then(resp => {
+    const dict = {};
+    let n = 1;
+    for (let i = 0; resp.data[i]; i++) {
+      if (i !== 0) {
+        if (resp.data[i].userId !== resp.data[i - 1].userId) {
+          n = 1;
+        }
+      }
+      if (resp.data[i].completed === true) {
+        dict[resp.data[i].userId] = n++;
+      }
+    }
+    console.log(dict);
+  });
