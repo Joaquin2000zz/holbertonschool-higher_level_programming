ee14597fdb57cae63df099e8e48fca2d6a9327f5
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 18:58:11 +0000

    changed ++ position to ingrement before the assingnment

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,15 +4,15 @@ const axios = require('axios');
 axios.get(argv[2])
   .then(resp => {
     const dict = {};
-    let n = 1;
+    let n = 0;
     for (let i = 0; resp.data[i]; i++) {
       if (i !== 0) {
         if (resp.data[i].userId !== resp.data[i - 1].userId) {
-          n = 1;
+          n = 0;
         }
       }
       if (resp.data[i].completed === true) {
-        dict[resp.data[i].userId] = n++;
+        dict[resp.data[i].userId] = ++n;
       }
     }
     console.log(dict);
