1dc28b438803129c4fb805a514dd5e6300024146
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-17 20:34:04 +0000

    now i'm requesting only the movie id

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,12 @@
 #!/usr/bin/node
 const argv = process.argv;
 const axios = require('axios');
-axios.get('https://swapi-api.hbtn.io/api/films/')
+axios.get('https://swapi-api.hbtn.io/api/films/' + argv[2] + '/')
   .then(resp => {
-    argv[2] = parseInt(argv[2]);
-    if (argv[2] >= 4) {
-      argv[2] -= 3;
-    } else {
-      argv[2] += 3;
-    }
-    for (let i = 0; resp.data.results[i]; i++) {
-      if (resp.data.results[i].episode_id === argv[2]) {
-        for (let j = 0; resp.data.results[i].characters[j]; j++) {
-          axios.get(resp.data.results[i].characters[j])
-            .then(resp => {
-              console.log(resp.data.name);
-            });
-        }
-        break;
-      }
+    for (let i = 0; resp.data.characters[i]; i++) {
+      axios.get(resp.data.characters[i])
+        .then(resp => {
+          console.log(resp.data.name);
+        });
     }
   });
