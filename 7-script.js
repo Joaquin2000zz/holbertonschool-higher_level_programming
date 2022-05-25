b0f6ac72562cc8781a651dbcc8ae6255aede3869
Joaquin2000zz <91090043+Joaquin2000zz@users.noreply.github.com>
2022-05-25 20:29:25 -0300

    Delete 7-script.js

--- 7-script.js
+++
@@ -1,8 +0,0 @@
-function httpGet(theUrl) {
-    let xmlHttpReq = new XMLHttpRequest();
-    xmlHttpReq.open("GET", theUrl, false); 
-    xmlHttpReq.send(null);
-    const ret = JSON.parse(xmlHttpReq.responseText);
-    return ret['name']
-  }
-$('header').text(httpGet('https://swapi-api.hbtn.io/api/people/5/?format=json'));
